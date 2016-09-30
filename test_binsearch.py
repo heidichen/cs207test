2129e6462349767ecd09f08ba13acc85e875a814
Heidi Chen <s.heidi.chen@gmail.com>
2016-09-30 09:51:01 -0400

    initial test

--- test_binsearch.py
+++ test_binsearch.py
@@ -5,15 +5,6 @@ def test_binsearch():
     input = list(range(10))
     assert binary_search(input) == 5
 
-def test_inputtype_left(self):
-    assertTrue(isinstance(left), numbers.int))
-
-def test_inputtype_right(self):
-    assertTrue(isinstance(right), numbers.int))
-
-def test_input_boundary(self):
-    asset right >= left
-
 def test_char_binsearch():
     with raises(TypeError):
         binary_search(['a', 3])
